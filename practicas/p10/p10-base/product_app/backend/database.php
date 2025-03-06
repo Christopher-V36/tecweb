f679f7018f8ef30abc043fac8f7fb5c3660f0a33
Chris36 <christopheraguilar5348@gmail.com>
2025-03-05 22:47:56 -0600

    Ejercicios 1 y 2

---
+++ practicas/p10/p10-base/product_app/backend/database.php
@@ -0,0 +1,15 @@
+<?php
+    $conexion = @mysqli_connect(
+        'localhost',
+        'root',
+        'id_php_buap',
+        'marketzone'
+    );
+
+    /**
+     * NOTA: si la conexión falló $conexion contendrá false
+     **/
+    if(!$conexion) {
+        die('¡Base de datos NO conextada!');
+    }
+?>
